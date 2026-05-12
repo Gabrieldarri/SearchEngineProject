5513f5714e623095183a26ff8991378104d67c35
Gabrieldarri <gabrielbjarnason@gmail.com>
2026-05-12 14:36:33 +0200

    33

--- Shared/Paths.cs
+++ Shared/Paths.cs
@@ -4,7 +4,7 @@ namespace Shared;
     {
         public static readonly string POSTGRES_DATABASE =
             Environment.GetEnvironmentVariable("POSTGRES_DATABASE") ??
-            "Server=127.0.0.1;Port=5432;User Id=postgres;Password=1234;Database=searchlarge";
+            "Server=127.0.0.1;Port=5433;User Id=postgres;Password=1234;Database=searchlarge";
 
         public static readonly string POSTGRES_DATABASE_2 =
             Environment.GetEnvironmentVariable("POSTGRES_DATABASE_2");
